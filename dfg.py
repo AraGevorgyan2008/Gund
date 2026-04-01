38a8653798cb8de26a429c7dafa8b477cdeae53e
Usanogh-9 <manebalbabyan735@gmail.com>
2026-04-01 10:26:13 +0400

    manes

---
+++ dfg.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age, pasport, race, place, gender, number,sitting_place):
+        self.name = name
+        self.age = age
+        self.pasport = pasport
+        self.race = race
+        self.place = place
+        self.gender = gender
+        self.number = number
+        self.sitting_place = sitting_place
