5aa115c077c62d9adb4581578b19ce3144be691f
Usanogh-8 <aragevorgyan071108@gmail.com>
2026-04-13 10:45:21 +0400

    .

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import csv
 
 class Person:
-    def __init__(self, name, age, pasport, race, place, gender, number,sitting_place):
+    def __init__(self, ID, name, age, passport, race, place, gender, number,sitting_place):
+        self.ID=ID
         self.name = name
         self.age = age
-        self.pasport = pasport
+        self.passport = passport
         self.race = race
         self.place = place
         self.gender = gender
@@ -12,6 +13,19 @@ class Person:
         self.sitting_place = sitting_place
 
     def add(self):
-        with open('data.csv', "a") as csvfile:
+        with open('data1.csv', "a") as csvfile:
             w = csv.writer(csvfile)
-            w.writerow([self.race, self.place, self.gender, self.number, self.sitting_place])
+            w.writerow([self.ID,self.name,self.age,self.passport,self.race, self.place, self.gender, self.number, self.sitting_place])
+
+    def __str__(self):
+        return f"Name {self.name}\n Age {self.age}\n Passport {self.passport}\n Race {self.race}\n Place {self.place}\n Gender {self.gender}\n Number {self.number}\n Sitting Place {self.sitting_place}"
+
+    def change(self):
+        with open('data1.csv', "r") as f:
+            data = f.readlines()
+            for i in data:
+
+
+a = Person(1,"fdf",25,"12599755","A3333","ddefdew","dfwe",947568555,"5A")
+a.add()
+a.change()
